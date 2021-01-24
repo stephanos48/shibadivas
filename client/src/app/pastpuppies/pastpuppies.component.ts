2d9af5736925c48624d4d2ba61afc041f7ec2c54
Stephen English <stephanos.english@gmail.com>
2021-01-23 21:31:41 -0600

    addmissingcommit

--- client/src/app/pastpuppies/pastpuppies.component.ts
+++ client/src/app/pastpuppies/pastpuppies.component.ts
@@ -12,6 +12,24 @@ export class PastpuppiesComponent implements OnInit {
   rachel: any = '../assets/img/rachel-sm.jpg';
   joey: any = '../assets/img/joey-sm1.jpg';
   zach: any = '../assets/img/zack-sm1.jpg';
+  zach3: any = '../assets/img/zack3.jpg';
+  zach4: any = '../assets/img/zack4.jpg';
+  zach5: any = '../assets/img/zack5.jpg';
+  zach6: any = '../assets/img/zack6.jpg';
+  pam: any = '../assets/img/pam3.jpg';
+  jim: any = '../assets/img/pam3.jpg';
+  dwight: any = '../assets/img/pam3.jpg';
+  angela: any = '../assets/img/pam3.jpg';
+  michael: any = '../assets/img/pam3.jpg';
+  pete: any = '../assets/img/pam3.jpg';
+  erin: any = '../assets/img/pam3.jpg';
+  adalind1: any = '../assets/img/adalind-small.jpg';
+  adalind: any = '../assets/img/adalind.jpg';
+  isabella: any = '../assets/img/isabella-small.jpg';
+  damon: any = '../assets/img/damon-small.jpg';
+  toby: any = '../assets/img/toby-small.jpg';
+  claire: any = '../assets/img/claire-small.jpg';
+  jimbo: any = '../assets/img/jimbo-samll.jpg';
 
   constructor() { }
 
